1df73c17019a776cad9eae1b123c8dcd855676fe
Hablna <145856383+Hablna@users.noreply.github.com>
2026-03-14 12:55:35 +0100

    TestIntegration deleted

--- Backend/src/test/java/com/alicedeveloppementweb/tp_automatisation_tests/service/BookServiceIntegrationTest.java
+++ Backend/src/test/java/com/alicedeveloppementweb/tp_automatisation_tests/service/BookServiceIntegrationTest.java
@@ -13,19 +13,4 @@ import java.util.List;
 public class BookServiceIntegrationTest {
     @Autowired
     private BookRepository bookRepository;
-
-    @Test
-    void should_get_all_books(){
-        //Arra
-        bookRepository.save(new Book(null, "1984", "Orwell", 1948));
-        bookRepository.save(new Book(null, "Le Meilleur des Mondes", "Huxley", 1932));
-
-        //Act
-        List<Book> books = bookRepository.findAll();
-
-        //Assert
-        assertEquals(2, books.size());
-        assertEquals("1984", books.getFirst().getTitre());
-        assertEquals("Huxley", books.getLast().getAuteur());
-    }
 }
